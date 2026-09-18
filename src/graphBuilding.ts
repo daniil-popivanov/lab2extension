d006f9095bd6304cb15a7a6bc1ff88c8a9fef788
daniil-popivanov <daniil.popivanov.dev@gmail.com>
2026-09-19 00:54:37 +0300

    First working version. GraphBuilding and textProcessing completed

--- src/graphBuilding.ts
+++ src/graphBuilding.ts
@@ -2,8 +2,23 @@ import * as vscode from 'vscode'
 import { processText } from './textProcessing.js'
 import { parseCode } from './filework.js'
 
+interface GraphVertex {
+    id:number;
+    label:string|string[];
+}
+
+interface GraphEdge {
+    from:number;
+    to:number;
+    arrows:string;
+}
+
 export class WebPanel {
     private panel:vscode.WebviewPanel|undefined;
+
+    constructor(
+        private readonly context: vscode.ExtensionContext
+    ) {}
 
     connectWebview():void {
         this.panel = vscode.window.createWebviewPanel(
@@ -11,22 +26,41 @@ export class WebPanel {
             'Graph',
             vscode.ViewColumn.One,
             {
-                enableScripts: false,
+                enableScripts: true,
+                localResourceRoots: [
+                    vscode.Uri.joinPath(
+                        this.context.extensionUri,
+                        'src',
+                        'media'
+                    )
+                ]
             }
         );
+
+        this.panel.onDidDispose(() => {
+            this.panel = undefined;
+        });
     }
 
-    makeGraph() {
+    async makeGraph(): Promise<void> {
         if (!this.panel){
             this.connectWebview();
         }
 
+        const visPath = vscode.Uri.joinPath(
+            this.context.extensionUri,
+            'src',
+            'media',
+            'vis-network.min.js'
+        );
+
+        const visUri = this.panel!.webview.asWebviewUri(visPath);
         let markedData:Map<string, string[]>|[string[], string] = processText(parseCode());
 
         if (Array.isArray(markedData)) {
             let isFixed:boolean = false;
             let attemp:number = 0;
-            const delay = (seconds: number) => new Promise(resolve => setTimeout(resolve, seconds * 1000));
+            const delay = (seconds: number):Promise<void> => new Promise(resolve => setTimeout(resolve, seconds * 1000));
 
             async function retryAttemp() {
                 console.log("Retry after 5 seconds...");
@@ -35,9 +69,10 @@ export class WebPanel {
             }
 
             console.log(markedData[0][0]);
+            const max_attemp:number = 5;
 
-            while (!isFixed && attemp < 10) {
-                retryAttemp();
+            while (!isFixed && attemp < max_attemp) {
+                await retryAttemp();
 
                 attemp++;
                 markedData = processText(parseCode());
@@ -57,21 +92,86 @@ export class WebPanel {
             }
         }
 
-        let htmlCode:string = `<!DOCTYPE html>
+        let htmlCode:string = `
+        <!DOCTYPE html>
         <html lang="en">
         <head>
+        
         <meta charset="UTF-8">
         <title>Your code graph</title>
+        
+        <meta
+            http-equiv="Content-Security-Policy"
+            content="
+                default-src 'none';
+                script-src ${this.panel!.webview.cspSource} 'unsafe-inline';
+                style-src 'unsafe-inline';
+            "
+        >
+            
+            <style>
+                #network-container {
+                    width: 1000px;
+                    height: 600px;
+                    border: 1px solid lightgray;
+                }
+            </style>
+
+            <script src="${visUri}"></script>
         </head>
         <body>
-        <h1>Your code graph</h1>
-        <p>Your code is empty/your graph ins't builded yet</p>
-        </body>
-        </html>`;
+            <div id="network-container"></div>
+        `;
+        let vertices:GraphVertex[] = [];
+        let edges:GraphEdge[] = [];
+        let vertexCheck:Map<string|string[], number> = new Map<string, number>;
 
         for (const [key, value] of markedData) {
-            continue;
+            if (!vertexCheck.has(key)) {
+                vertexCheck.set(key, vertices.length + 1);
+                const vrtx:GraphVertex = {id:vertices.length + 1, label:key};
+                vertices.push(vrtx);
+            }
+
+            for (const cur_value of value){
+                if (!vertexCheck.has(cur_value)) {
+                    vertexCheck.set(cur_value, vertices.length + 1);
+                    const vrtx:GraphVertex = {id:vertices.length + 1, label:cur_value};
+                    vertices.push(vrtx);
+                }
+    
+                const edge:GraphEdge = {from:vertexCheck.get(key)!, to:vertexCheck.get(cur_value)!, arrows:'to'};
+                edges.push(edge);
+            }
         }
+        
+        htmlCode += `
+            <script>
+                const vertices = ${JSON.stringify(vertices)};
+                const edges = ${JSON.stringify(edges)};
+
+                const container = document.getElementById('network-container');
+
+                const nodes = new vis.DataSet(vertices);
+                const networkEdges = new vis.DataSet(edges);
+                new vis.Network(
+                    container,
+                    { nodes, edges: networkEdges },
+                    {
+                        edges: {
+                            color: '#5780afc5',
+                            width: 2
+                        },
+                        physics: {
+                            stabilization: true
+                        }
+                    }
+                );
+                console.log(typeof vis);
+            </script>
+        </body>
+        </html>
+        `;
 
         this.panel!.webview.html = htmlCode;
     }
--- src/textProcessing.ts
+++ src/textProcessing.ts
@@ -34,62 +34,141 @@ export function processText(data:[string[], string]):Map<string, string[]>|[stri
     }
 
     const patterns: Map<RegExp, string> = new Map();
-    patterns.set(/\bstruct\b .+\s*{/, 'structure');
+    patterns.set(/\bstruct\b .+\s*\{/, 'structure');
     patterns.set(/\bclass\b .+\s*\{/ , 'class');
     patterns.set(/.+::.+\(.*\)\s*\{/, 'class_method_out');
     patterns.set(/if\s*\(.+\)\s*\{/, 'if');
+    patterns.set(/\}\s*else\s*\{/, 'else_closed');
+    patterns.set(/else\s*\{/, 'else');
     patterns.set(/while\s*\(.+\)\s*\{/, 'while');
     patterns.set(/for\s*\(.+\)\s*\{/, 'for');
     patterns.set(/switch\s*\(.+\)\s*\{/, 'switch');
+    patterns.set(/return\s*.+\(.*\);/, 'return_func');
     patterns.set(/.+ .+\(.*\)\s*\{/, 'function/class_method_in');
-    patterns.set(/return\s*.+\(\s*\);/, 'return_func');
     patterns.set(/\s*}\s*/, 'closed');
+    patterns.set(/\s*\{\s*/, 'open');
+    
+    const patterns_without_bracket: Map<RegExp, string> = new Map();
+    patterns_without_bracket.set(/\bstruct\b .+\s*/, 'structure');
+    patterns_without_bracket.set(/\bclass\b .+\s*/ , 'class');
+    patterns_without_bracket.set(/.+::.+\(.*\)\s*/, 'class_method_out');
+    patterns_without_bracket.set(/if\s*\(.+\)\s*/, 'if');
+    patterns_without_bracket.set(/\}\s*else\s*\{/, 'else_closed');
+    patterns_without_bracket.set(/else\s*/, 'else');
+    patterns_without_bracket.set(/while\s*\(.+\)\s*/, 'while');
+    patterns_without_bracket.set(/for\s*\(.+\)\s*/, 'for');
+    patterns_without_bracket.set(/switch\s*\(.+\)\s*/, 'switch');
+    patterns_without_bracket.set(/.+ .+\(.*\)\s*/, 'function/class_method_in');
+    
     let markers: Map<string, string[]> = new Map<string, string[]>;
     let brackets:Stack<string> = new Stack<string>;
     let current_struct:Stack<string> = new Stack<string>;
     let names_of_struct:Stack<string> = new Stack<string>;
-
+    
     for (let i = 0; i < data[0].length; i++) {
         let typeOfStr:string = '';
-
+        let isOpen:boolean = false;
+        
         for (const [key, value] of patterns) {
             if (key.test(data[0][i])) {
                 typeOfStr = value;
                 break;
             }
         }
-
+        
         if (typeOfStr == ''){
             continue;
         }
 
-        if (typeOfStr == 'function/class_method_in'){
-            const name = data[0][i].match(/.+ .+\(.*\)\s*\{/)![0].split(' ')[1].split('(')[0];
+        if (typeOfStr == 'open') {
+            isOpen = true;
 
-            if (current_struct.top() == 'class') {
-                markers.get(names_of_struct.top())!.push(`${names_of_struct.top()}::` + name);
+            for (const [key, value] of patterns_without_bracket) {
+                if (key.test(data[0][i - 1])) {
+                    typeOfStr = value;
+                    break;
+                }
             }
-            else if (current_struct.top() == 'function/class_method_in'){
+
+            i--;
+        }
+
+        if (typeOfStr == 'function/class_method_in'){
+            const name:string = data[0][i].split(' ').filter(word => /.+\(.*/.test(word))[0].split('(')[0];
+
+            if (current_struct.isEmpty()){
+                markers.set(name + '()', []);
+            }
+            else{
+                if (current_struct.top() == 'class' || current_struct.top() == 'structure') {
+                    markers.get(names_of_struct.top())!.push(`${names_of_struct.top()}::` + name + '()');
+                }
+                else if (current_struct.top() == 'function/class_method_in'){
+                    if (!markers.has(names_of_struct.top())){
+                        markers.set(names_of_struct.top() + '()', []);
+                    }
+
+                    markers.get(names_of_struct.top())!.push(name + '()');
+                }
+            }
+
+            names_of_struct.push(name + '()');
+        }
+        else if (typeOfStr == 'return_func') {
+            const name:string = data[0][i].split(' ').filter(word => /.+\(.*/.test(word))[0].split('(')[0];
+
+            if (markers.has(names_of_struct.top())) {
+                markers.get(names_of_struct.top())!.push(name + '()');
+            }
+            else {
                 markers.set(names_of_struct.top(), []);
+                markers.get(names_of_struct.top())!.push(name + '()');
+            }
+        }
+        else if (typeOfStr == 'class' || typeOfStr == 'structure'){
+            let name:string;
+
+            if (/\s*\{\s*/.test(data[0][i])){
+                name = data[0][i].split(' ')[1];
+            }
+            else{
+                name = data[0][i].split(' ')[1].split('{')[0];
+            }
+            
+            if (!current_struct.isEmpty() && current_struct.top() == 'class') {
                 markers.get(names_of_struct.top())!.push(name);
             }
 
+            markers.set(name, []);
             names_of_struct.push(name);
         }
-        else if (typeOfStr == 'return_func') {
-            //придумать, как закидывать функции, которые содержат в себе другие функции и при этом сами сидят в других функциях/классах
-        }
-        else if (typeOfStr == 'class'){
-            const name = data[0][i].match(/.+ .+\(.*\)\s*\{/)![0].split(' ')[1].split('{')[0];
-            
-            if (markers.size != 0) {
-                markers.get(current_struct.top())!.push(name)
-            }
+        else if (typeOfStr == 'class_method_out') {
+            const name:string = data[0][i].split(' ').filter(word => /.+::.+\(.*/.test(word))[0].split('(')[0];
+            const class_name:string = name.split('::')[0];
 
+            if (!markers.has(class_name)){
+                markers.set(class_name, []);
+            }
+            
+            markers.get(class_name)!.push(name + '()');
             names_of_struct.push(name);
-        }    
+        }
 
         switch (typeOfStr) {
+            case 'else_closed':
+                if (!(brackets.top() == '{i')) {
+                    current_struct.pop();
+                    names_of_struct.pop();
+                }
+                
+                if (brackets.isEmpty()){
+                    brackets.push('}');
+                    brackets.push('}');
+                }
+                
+                brackets.pop();
+                brackets.push('{i');
+                break;
             case 'function/class_method_in':
             case 'class_method_out':
             case 'structure':
@@ -97,11 +176,35 @@ export function processText(data:[string[], string]):Map<string, string[]>|[stri
                 current_struct.push(typeOfStr);
                 brackets.push('{');
                 break;
-            default:
-                current_struct.pop();
+            case 'if':
+            case 'else':
+            case 'switch':
+            case 'while':
+            case 'for':
+                brackets.push('{i');
+                break;
+            case 'closed':
+                if (!(brackets.top() == '{i')) {
+                    current_struct.pop();
+                    names_of_struct.pop();
+                }
+
+                if (brackets.isEmpty()){
+                    brackets.push('}');
+                    brackets.push('}');
+                }
+
                 brackets.pop();
                 break;
         }
+
+        if (isOpen) {
+            i++;
+        }
+    }
+
+    if (!brackets.isEmpty() || !current_struct.isEmpty() || !names_of_struct.isEmpty()) {
+        return [["Error, something gone wrong! Brackets is not dual!"], "1"];
     }
 
     return markers;
